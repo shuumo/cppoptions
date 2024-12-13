7427b85f8363dc542f4020ab5b6d0ba9ef7accf4
shuumo <arnav.divesh.singh@gmail.com>
2024-12-13 14:00:36 +1100

    random number generation using standard library

--- montecarlotest/montecarlotest.cpp
+++ montecarlotest/montecarlotest.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
 
 int main(int argc, char **argv) {
+    std::vector<int> randomNumbers;
 
-    for(int i = 0; i < 5; i++) {
-        std::cout << "hello world" << std::endl;
+    for(int i = 0; i < 10; i++) {
+       randomNumbers.push_back(std::rand()); 
+    }
+    
+    std::sort(randomNumbers.begin(), randomNumbers.end());
+
+    for(const auto& i : randomNumbers) {
+        std::cout << "number: " << i << std::endl;
     }
 
     return 0;
-
 }
