e382fc4fe6a702e689b85b35ab590ec551556223
shuumo <arnav.divesh.singh@gmail.com>
2024-12-16 21:39:05 +1100

    set up modifyable parameters for number of simulations, initial stock price, time. sigma, mu etc. for the xtensor matplotlibcpp c++ GBM Simulator

--- montecarlotest/gbmcpp/geo_brownian_motion_sim.cpp
+++ montecarlotest/gbmcpp/geo_brownian_motion_sim.cpp
@@ -9,23 +9,19 @@
 #include <cmath>
 #include <ctime>
 #include <vector>
+#include <chrono>
 
 namespace plt = matplotlibcpp;
 
 // modifyable parameters
-#define MU 0.04           //(%)
-#define SIGMA 0.18        //(%)
-#define NUM_OF_YEARS 2    //(years)
+#define SIM_COUNT 5       //Number of GBM simulations to plot
+#define MU 0.04           //(%) drift over years
+#define SIGMA 0.18        //(%) volatility over years
+#define NUM_OF_YEARS 1    //(years)
+#define START_PRICE 200   //($) initial prices of simulaitons
 
-int main(int argc, char **argv) { 
-    xt::random::seed(time(nullptr));
 
-    // drift (over years) 
-    double mu = MU;
-     
-    // volatility (over years)
-    double sigma = SIGMA;
-
+std::vector<double> createGbmSimXValues(int years, double mu, double sigma, double startprice) {      
     // change in time (years)
     double dt = 1.0/365.0;
 
@@ -36,34 +32,40 @@ int main(int argc, char **argv) {
     double s = sigma;
     
     // daily log returns
-    auto logreturn = xt::random::randn({(1/dt)*NUM_OF_YEARS}, m*dt, s * std::sqrt(dt));
+    auto logreturn = xt::random::randn({(1/dt)*years}, m*dt, s * std::sqrt(dt));
 
     // daily raw returns 
     auto returns = xt::exp(logreturn) - 1;
 
-    // daily prices assuming S0=1
-    auto price = xt::exp(xt::cumsum(logreturn));
-    
-    
-    std::cout << "mu: " << mu << std::endl;
-    std::cout << "sigma: " << sigma << std::endl;
-    std::cout << "dt: " << dt << std::endl;
-    std::cout << "m: " << m << std::endl;
-    std::cout << "s: " << s << std::endl; 
-    
-    std::vector<double> changeInPrice(price.begin(), price.end()); 
-    std::cout << "time change of simulation: " << changeInPrice.size() << std::endl; 
+    // daily prices when S0=1
+    auto price = xt::exp(xt::cumsum(logreturn)) * startprice;
 
-    auto times = xt::linspace<double>(0., (double) changeInPrice.size(), changeInPrice.size());
-    std::vector<double> changeInTime(times.begin(), times.end());
+    std::vector<double> pricesVector(price.begin(), price.end());
+    return pricesVector;
+}
+
+
+
+int main(int argc, char **argv) { 
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    xt::random::seed(seed);
+
+    std::vector<std::vector<double>> simulationList;
+    for(int simNumber = 0; simNumber < SIM_COUNT; simNumber++) {
+        simulationList.push_back(
+                createGbmSimXValues(NUM_OF_YEARS, MU, SIGMA, START_PRICE)
+                ); 
     
-    for(const auto& i : changeInPrice) {
-        std::cout << i << std::endl;
+    }
+    int lengthOfChange = simulationList[0].size();
+    auto times = xt::linspace<double>(0., (double) lengthOfChange, lengthOfChange);
+    std::vector<double> changeInTime(times.begin(), times.end()); 
+
+    for(const auto& sim : simulationList) {
+        plt::plot(changeInTime, sim);
     }
 
-    plt::plot(changeInTime, changeInPrice);
     plt::show();
-
     return 0;
 }
 
